a5aa623fa853a633cda823a27b7f4cd185bb1b69
Stackbit Code Editor <projects@stackbit.com>
2021-05-04 17:59:37 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Page from './[...slug]';
 
 export async function getStaticProps({ params }) {
     console.log('Page [index] getStaticProps, params: ', params);
-    //aabbccdddeeff
+    //aabbccdddeeffgg
     const props = await sourcebitDataClient.getStaticPropsForPageAtPath('/');
     return { props };
 }
