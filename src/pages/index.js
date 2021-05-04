965bb6af94aee8b1aeb23375bdb7065785426a21
Stackbit Code Editor <projects@stackbit.com>
2021-05-04 18:16:23 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { sourcebitDataClient } from 'sourcebit-target-next';
  */
 
 import Page from './[...slug]';
-//ddd
+//dddfdf
 
 export async function getStaticProps({ params }) {
     console.log('Page [index] getStaticProps, params: ', params);
